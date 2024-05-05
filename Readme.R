c3843428f64e141422317098c8e4210a02981186
Kalicz Péter <kaliczp@gmail.com>
2024-05-05 18:51:46 +0200

    Extract Tes cell

--- Readme.R
+++ Readme.R
@@ -1,3 +1,4 @@
 ## Beolvasás unz connecitonnal
 zipgrid <- unz("sr_grid_20012023.txt.zip", "sr_grid_20012023.txt")
 alldata <- read.fwf(zipgrid, widths = rep(8, 1234), skip = 1, fill = TRUE)
+TesGrid <- alldata[,c(1,312)]
