47719644d3c477259b5cc7fccfa788e747df0b2b
Kalicz Péter <kaliczp@gmail.com>
2024-05-05 18:59:37 +0200

    Export all data for one grid

--- Readme.R
+++ Readme.R
@@ -4,4 +4,4 @@ alldata <- read.fwf(zipgrid, widths = rep(8, 1234), skip = 1, fill = TRUE)
 TesGrid <- alldata[,c(1,312)]
 GridDate <- seq.Date(from = as.Date("2001-01-01"), to = as.Date("2023-12-31"), by = "days")
 plot(GridDate, TesGrid[,2], type = "l")
-
+write.table(data.frame(GridDate, TesGrid[,2]), "TesGlobal.csv", sep = ";", dec = ",", eol = "\r\n", row.names = FALSE)
